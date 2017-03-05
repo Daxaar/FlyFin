2ddf5afd749f98d7fde690981fcfad10a885fba3
Darren Lewis <darren@octono.com>
2017-03-05 16:53:01 +0000

    Add random image to home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  _image : string = null;
+
   constructor(public navCtrl: NavController) {
 
+  }
+  randomImage() {
+
+      if (this._image !== null) {
+          return this._image;
+      }
+
+      if (Math.random() > 0.5) {
+          this._image = "assets/a380.jpg";
+      } else {
+        this._image = "assets/flyfin.jpg";
+      }
+      return this._image;
   }
 
 }
