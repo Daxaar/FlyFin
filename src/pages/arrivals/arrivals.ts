3b5416dca91ce77a991b5e0559cbb91f29531790
Darren Lewis <darren@octono.com>
2017-03-06 20:33:36 +0000

    Add arrivals listing

--- src/pages/arrivals/arrivals.ts
+++ src/pages/arrivals/arrivals.ts
@@ -1,22 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the Arrivals page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-arrivals',
   templateUrl: 'arrivals.html'
 })
 export class ArrivalsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    flights: any;
+    arrivals: any;
+    private _arrived: boolean = false;
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
+        this.getArrivals();
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ArrivalsPage');
   }
 
+  get arrived(): boolean {
+      return this._arrived;
+  }
+  set arrived(value: boolean) {
+      this._arrived = value;
+  }
+
+  isOnApproach(flight) {
+      return flight.Status === "On Approach";
+  }
+
+  getArrivals(refresher: any = null) {
+
+      this.http.get("http://daxaar.ddns.net:3001/flights/arrivals")
+          .map(res => res.json())
+          .subscribe(data => {
+              this.flights = data.arrivals;
+              refresher && refresher.complete();
+          },
+          error => console.log("error! : ", error));
+  }
 }
