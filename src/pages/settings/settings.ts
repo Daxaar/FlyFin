f57e0bfae06d69b47dd327c918e4432f80bbee92
Darren Lewis <darren@octono.com>
2017-04-09 21:57:04 +0100

    Settings page initial bindings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -13,10 +13,45 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private _settings = new Array<Setting>();
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this._settings.push(new Setting("Sort by expected",true));
+    this._settings.push(new Setting("Show arrived", false));
+    this._settings.push(new Setting("Show departed", false));
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
   }
 
+  get settings(): Array<Setting> {
+    return this._settings;
+  }
+
+}
+
+export class Setting {
+
+    private _text:string;
+    private _selected:boolean;
+
+    constructor(text: string, selected: boolean ){
+      this._text = text;
+      this._selected = selected;
+    }
+
+    get text(): string {
+      return this._text;
+    }
+    set text(value: string){
+      this._text = value;
+    }
+    get selected(): boolean {
+      return this._selected;
+    }
+    set selected(value: boolean){
+      console.log(`${this._text} - ${value}`);
+      this._selected = value;
+    }
 }
